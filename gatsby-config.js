b8640687574ccc214cac49739b60474a716766e6
sellerg <gregoryseller1@gmail.com>
2020-07-10 12:38:53 +0100

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,16 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
