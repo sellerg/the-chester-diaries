3636b9cbde9514c49441dd2c402939aa76731f74
sellerg <gregoryseller1@gmail.com>
2020-07-10 16:00:34 +0100

    added globalStyles

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,24 @@
-import {ThemeProvider} from "styled-components";
+import {createGlobalStyle, ThemeProvider} from "styled-components";
 import Theme from "./src/themes/theme";
 import React from "react";
 
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1};
+    }
+`;
+
 export const wrapRootElement = ({element}) => (
     <ThemeProvider theme={Theme}>
+        <GlobalStyles />
         {element}
     </ThemeProvider>
 )
