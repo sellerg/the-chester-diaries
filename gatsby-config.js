457c77091b5c4ff96f080c9776419a76a39d45b8
sellerg <gregoryseller1@gmail.com>
2020-07-10 12:30:34 +0100

    gatsby-mdx & gatsby-sharp plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,20 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
