e48642d60dd8d79be0548a081340b2d938d14fc0
sellerg <gregoryseller1@gmail.com>
2020-07-10 14:33:33 +0100

    added posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  siteMetadata: {
-    title: `The Chester Diaries`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -51,18 +45,6 @@ module.exports = {
           `muli\:400,400i,700,700i`,
         ],
         display: "swap",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
