885b93361d2b7fe3aadea468fe07291ac23ccfd0
sellerg <gregoryseller1@gmail.com>
2020-07-14 16:05:48 +0100

    create single post page

--- gatsby-node.js
+++ gatsby-node.js
@@ -32,17 +32,16 @@ exports.createPages = async function ({ actions, graphql }) {
       },
     })
   })
+
+  //Create single blog post
+
+  data.allMdx.edges.forEach(edge => {
+    const slug = edge.node.frontmatter.slug
+    const id = edge.node.id
+    actions.createPage({
+      path: slug,
+      component: require.resolve(`./src/templates/singlePost.js`),
+      context: { id },
+    })
+  })
 }
-
-//Create single blog post
-
-//   data.allMdx.edges.forEach(edge => {
-//     const slug = edge.node.frontmatter.slug
-//     const id = edge.node.id
-//     actions.createPages({
-//       path: slug,
-//       component: require.resolve(`./src/templates/singlePost.js`),
-//       context: { id },
-//     })
-//   })
-// }
