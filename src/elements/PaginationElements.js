e19dc4b67c575d1abac1bec74384560b40550aa6
sellerg <gregoryseller1@gmail.com>
2020-07-14 15:01:28 +0100

    create all posts graphql

--- src/elements/PaginationElements.js
+++ src/elements/PaginationElements.js
@@ -9,14 +9,14 @@ export const PaginationWrapper = styled.div`
   align-items: center;
   justify-content: center;
 
-  &a::nth-child(1) {
+  a:nth-child(1) {
     color: ${props =>
       props.isFirst ? props.theme.colors.dark3 : props.theme.colors.dark1};
     pointer-events: ${props => (props.isFirst ? "none" : "auto")};
     cursor: ${props => (props.isFirst ? "default" : "pointer")};
   }
 
-  &a::nth-child(2) {
+  a:nth-child(2) {
     color: ${props =>
       props.isLast ? props.theme.colors.dark3 : props.theme.colors.dark1};
     pointer-events: ${props => (props.isLast ? "none" : "auto")};
